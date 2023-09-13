23054ec3d2ed704b5a7c3e3e7f261eac612e9ff6
Foster808 <lafoster@outlook.com>
2023-09-12 17:01:44 -0700

    working blackjack

--- BlackJack Game/blackjack.py
+++ BlackJack Game/blackjack.py
@@ -1,46 +1,9 @@
 import random
 from random import choice
 from art import logo
-from replit import clear
 
 
-def deal_card():
-    """Returns a random card from the deck."""
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = random.choice(cards)
-    return card
-
-
-def calculate_score(cards):
-    """Take a list of cards and return the score calculated from the cards"""
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
-    if 11 in cards and sum(cards) > 21:
-        cards.remove(11)
-        cards.append(1)
-    return sum(cards)
-
-
-def compare(user_score, computer_score):
-    if user_score > 21 and computer_score > 21:
-        return "You went over. You lose!!!"
-    if user_score == computer_score:
-        return "Draw :("
-    elif computer_score == 0:
-        return "You Lose! Opponent has Blackjack."
-    elif user_score == 0:
-        return "You win with a Blackjack!"
-    elif user_score > 21:
-        return "You went over. You lose!!!"
-    elif computer_score > 21:
-        return "Opponent went over. You win!!!"
-    elif user_score > computer_score:
-        return "You win!!!"
-    else:
-        return "You lose!!!"
-
-
-def play_game():
+def main():
 
     print(logo)
 
@@ -77,4 +40,44 @@ def play_game():
 
     while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
         os.system("cls")
-        play_game()
+        main()
+
+
+def deal_card():
+    # Returns a random card from the deck
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+def calculate_score(cards):
+    # Take a list of cards and return the score calculated from the cards
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+
+def compare(user_score, computer_score):
+    if user_score > 21 and computer_score > 21:
+        return "You went over. You lose!!!"
+    if user_score == computer_score:
+        return "Draw :("
+    elif computer_score == 0:
+        return "You Lose! Opponent has Blackjack."
+    elif user_score == 0:
+        return "You win with a Blackjack!"
+    elif user_score > 21:
+        return "You went over. You lose!!!"
+    elif computer_score > 21:
+        return "Opponent went over. You win!!!"
+    elif user_score > computer_score:
+        return "You win!!!"
+    else:
+        return "You lose!!!"
+
+
+if __name__ == "__main__":
+    main()
